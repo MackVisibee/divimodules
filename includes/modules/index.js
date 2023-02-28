e48d358e60f5c1529b6f745b8fd43d2a4702d8f7
MackVisible <mack@visibleone.com>
2023-02-28 16:48:43 +0630

    first commit

---
+++ includes/modules/index.js
@@ -0,0 +1,37 @@
+import HelloWorld from './HelloWorld/HelloWorld';
+import MCBlogGrid from './MCBlogGrid/MCBlogGrid';
+import MCFlipBoxChild from './MCFlipBoxChild/MCFlipBoxChild';
+import MCFlipBoxParent from './MCFlipBoxParent/MCFlipBoxParent';
+import OurTeamParent from './OurTeamParent/OurTeamParent';
+import OurTeamChild from './OurTeamChild/OurTeamChild';
+import CustomFlix  from './CustomFlix/CustomFlix';
+import CustomFlixChild from './CustomFlixChild/CustomFlixChild';
+
+import CustomVideoParent from './CustomVideoParent/CustomVideoParent';
+import CustomVideoChild from './CustomVideoChild/CustomVideoChild';
+
+import HomeBanner from './HomeBanner/HomeBanner';
+import ScrollSections from './ScrollSections/ScrollSections';
+import MemberCardParent from './MemberCardParent/MemberCardParent';
+import MemberCardChild from './MemberCardChild/MemberCardChild';
+import AccordionToggleParent from './AccordionToggleParent/AccordionToggleParent';
+import AccordionToggleChild from './AccordionToggleChild/AccordionToggleChild';
+
+export default [ 
+    HelloWorld,
+    MCBlogGrid,
+    MCFlipBoxChild,
+    MCFlipBoxParent,
+    OurTeamParent,
+    OurTeamChild,
+    CustomFlix,
+    CustomFlixChild,
+    HomeBanner,
+    ScrollSections,
+    CustomVideoParent,
+    CustomVideoChild,
+    MemberCardParent,
+    MemberCardChild,
+    AccordionToggleParent,
+    AccordionToggleChild
+];
